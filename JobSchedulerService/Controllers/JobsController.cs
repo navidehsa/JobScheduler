4d61a66824afba570a23b98f4594d72146cd7c65
Navideh <nasaghiri@gmail.com>
2023-03-13 15:10:41 +0100

    Add endpoint to create and get jobs

---
+++ JobSchedulerService/Controllers/JobsController.cs
@@ -0,0 +1,55 @@
+﻿using JobSchedulerService.Managers;
+using JobSchedulerService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace JobSchedulerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly IJobManager _jobManager;
+
+        public JobsController(IJobManager jobManager)
+        {
+            _jobManager = jobManager;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(
+        Summary = "Enqueue and create a new job, by providing an unsorted array of numbers as input.",
+        OperationId = "CreateJob"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "Request Successful", typeof(string))]
+        public IActionResult CreateJob(int[] input)
+        {
+            var jobId = _jobManager.CreateJob(input);
+            return Ok(jobId);
+        }
+
+        [HttpGet]
+        [SwaggerOperation(
+        Summary = "Fetches all enqueued job.",
+        OperationId = "GetAllJobs"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "Request Successful", typeof(IEnumerable<Job>))]
+        public IActionResult GetAllJobs()
+        {
+            var jobs = _jobManager.GetAllJobs();
+            return Ok(jobs);
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerOperation(
+        Summary = "Fetches information about a specific job and returns the job status and metadata.",
+        OperationId = "GetJob"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "Request Successful", typeof(Job))]
+        public IActionResult GetJob(string id)
+        {
+            var job = _jobManager.GetJob(id);
+            return Ok(job);
+        }
+    }
+}
--- JobSchedulerService/Program.cs
+++ JobSchedulerService/Program.cs
@@ -1,8 +1,26 @@
+using Hangfire.MemoryStorage;
+using Hangfire;
+using JobSchedulerService.Managers;
+using JobSchedulerService.Repository;
+using JobSchedulerService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Hangfire
+GlobalConfiguration.Configuration.UseMemoryStorage();
+builder.Services.AddHangfire(c => c.UseMemoryStorage());
+JobStorage.Current = new MemoryStorage();
+builder.Services.AddHangfireServer();
+
+//Services
+builder.Services.AddSingleton<IJobManager, JobManager>();
+builder.Services.AddSingleton<IJobRepository, JobRepository>();
+builder.Services.AddSingleton<ISortingService, SortingService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +35,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseHangfireDashboard();
+app.MapHangfireDashboard();
 
 app.UseAuthorization();
 
