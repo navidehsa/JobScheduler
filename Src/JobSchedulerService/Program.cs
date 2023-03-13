3700e8d326aad556183aa79a20ce355db220e4c3
Navideh <nasaghiri@gmail.com>
2023-03-13 23:01:17 +0100

    Restructure project

--- Src/JobSchedulerService/Program.cs
+++ Src/JobSchedulerService/Program.cs
@@ -31,7 +31,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Job Scheduler API v1");
+        c.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
